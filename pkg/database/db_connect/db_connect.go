a672d836db72ce0b7fc19bb38e4a03f4ad87e830
phn00dev <hudayberdipolat@gmail.com>
2024-11-11 11:42:52 +0500

    ready db connection

--- pkg/database/db_connect/db_connect.go
+++ pkg/database/db_connect/db_connect.go
@@ -1,1 +1,42 @@
 package dbconnect
+
+import (
+	"fmt"
+	"time"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+
+	"github.com/phn00dev/go-task-manager/pkg/config"
+)
+
+type DBConnect struct {
+	config *config.Config
+}
+
+func NewDBConnnect(config *config.Config) DBConnect {
+	return DBConnect{
+		config: config,
+	}
+}
+
+func (dbConnect DBConnect) GetDBConnect() (*gorm.DB, error) {
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s TimeZone=%s",
+		dbConnect.config.DbConfig.DbHost,
+		dbConnect.config.DbConfig.DbUser,
+		dbConnect.config.DbConfig.DbPassword,
+		dbConnect.config.DbConfig.DbName,
+		dbConnect.config.DbConfig.DbPort,
+		dbConnect.config.DbConfig.DbSllMode,
+		dbConnect.config.DbConfig.DbTimeZone,
+	)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		NowFunc: func() time.Time {
+			return time.Now().Local()
+		},
+	})
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
