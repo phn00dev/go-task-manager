b662a347dde69f15ed77e07310130ccf56d7103a
phn00dev <hudayberdipolat@gmail.com>
2024-11-11 11:46:01 +0500

    ready custom http

--- pkg/http_client/http_client.go
+++ pkg/http_client/http_client.go
@@ -1,1 +1,19 @@
 package httpclient
+
+import (
+	"net/http"
+	"time"
+)
+
+func NewHttpClient() *http.Client {
+	return &http.Client{
+		Timeout: 15 * time.Second,
+	}
+}
+
+func NewHttpServer() *http.Server {
+	return &http.Server{
+		ReadTimeout:  15 * time.Second,
+		WriteTimeout: 15 * time.Second,
+	}
+}
