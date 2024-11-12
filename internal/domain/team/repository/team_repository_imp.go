921722bb2d1408b0fec73f7e42cfe26a40402f26
phn00dev <hudayberdipolat@gmail.com>
2024-11-12 12:09:28 +0500

    team repo ready

--- internal/domain/team/repository/team_repository_imp.go
+++ internal/domain/team/repository/team_repository_imp.go
@@ -4,7 +4,6 @@ import (
 	"gorm.io/gorm"
 
 	"github.com/phn00dev/go-task-manager/internal/models"
-
 )
 
 type teamRepositoryImp struct {
@@ -18,21 +17,29 @@ func NewTeamRepository(db *gorm.DB) TeamRepository {
 }
 
 func (teamRepo teamRepositoryImp) GetAll() ([]models.Team, error) {
-	panic("team repo imp")
+	var teams []models.Team
+	if err := teamRepo.DB.Find(&teams).Error; err != nil {
+		return nil, err
+	}
+	return teams, nil
 }
 
 func (teamRepo teamRepositoryImp) GetOne(teamID int) (*models.Team, error) {
-	panic("team repo imp")
+	var team models.Team
+	if err := teamRepo.DB.First(&team, teamID).Error; err != nil {
+		return nil, err
+	}
+	return &team, nil
 }
 
 func (teamRepo teamRepositoryImp) Create(team models.Team) error {
-	panic("team repo imp")
+	return teamRepo.DB.Create(&team).Error
 }
 
 func (teamRepo teamRepositoryImp) Update(teamID int, team models.Team) error {
-	panic("team repo imp")
+	return teamRepo.DB.Model(&models.Team{}).Where("id=?", teamID).Updates(&team).Error
 }
 
 func (teamRepo teamRepositoryImp) Delete(teamID int) error {
-	panic("team repo imp")
+	return teamRepo.DB.Delete(&models.Team{}, teamID).Error
 }
