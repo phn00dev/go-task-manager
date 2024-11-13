b50f871fa516df4b3dbc9aa4cbd5a476b514f1a8
phn00dev <hudayberdipolat@gmail.com>
2024-11-13 09:04:10 +0500

    edited database tables and add new admins table

--- pkg/database/migrations/go_task_manager.sql
+++ pkg/database/migrations/go_task_manager.sql
@@ -1,44 +1,66 @@
+CREATE TYPE ADMIN_ROLE AS ENUM('super_admin', 'admin');
+CREATE TYPE STATUS AS ENUM('active', 'passive');
+CREATE TYPE TODO_STATUS AS ENUM ('new', 'working', 'done', 'paused', 'cancelled');
+
+-- admin table
+CREATE TABLE admins (
+    id SERIAL PRIMARY KEY, 
+    firtname VARCHAR(100) NOT NULL,
+    lastname VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password_hash VARCHAR(255) NOT NULL,
+    admin_role ADMIN_ROLE DEFAULT 'admin',
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    last_login TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+);  
+
+-- user table
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
+    firstname VARCHAR(100) NOT NULL,
+    lastname VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
-    password VARCHAR(100) NOT NULL,
+    password_hash VARCHAR(100) NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     last_login TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
+-- team table
 CREATE TABLE teams (
     id SERIAL PRIMARY KEY,
     team_name VARCHAR(100) UNIQUE,
+    team_slug VARCHAR(120) UNIQUE,
+    team_status STATUS DEFAULT 'passive', 
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
--- user_teams (many-to-many relationship between User and Team)
+-- user_teams table (many-to-many relationship between User and Team)
 CREATE TABLE user_teams (
+    id SERIAL PRIMARY KEY, 
     user_id INT NOT NULL,
     team_id INT NOT NULL,
-    PRIMARY KEY (user_id, team_id),
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
     FOREIGN KEY (team_id) REFERENCES teams(id) ON DELETE CASCADE
 );
 
-CREATE TYPE todo_status AS ENUM ('new', 'working', 'done', 'paused', 'cancelled', 'overdue');
-
+-- todos table
 CREATE TABLE todos (
     id SERIAL PRIMARY KEY,
     todo_title  VARCHAR(255) NOT NULL,
+    todo_slug VARCHAR(300) NOT NULL UNIQUE,
     todo_description TEXT,
-    todo_status todo_status DEFAULT 'new',
+    todo_status TODO_STATUS DEFAULT 'new',
     user_id INTEGER NOT NULL,
     team_id INTEGER,
-    deadline TIMESTAMP,
+    deadline TIMESTAMP DEFAULT NULL,
     completed_at TIMESTAMP DEFAULT NULL, 
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
-    FOREIGN KEY (team_id) REFERENCES teams(id) ON DELETE SET NULL
+    FOREIGN KEY (team_id) REFERENCES teams(id) ON DELETE CASCADE,
 );
 
 CREATE TABLE comments (
